05cb5d8acd5a34aa14aa9c5763d592c39249c746
Jakub Pomykala <jakub.pomykala@hotmail.co.uk>
2022-09-25 20:53:38 +0100

    TFW: converting SimpleBusImpl main() to SimpleBusImplTest.java

---
+++ yoko-testify/src/test/java/testify/bus/SimpleBusImplTest.java
@@ -0,0 +1,125 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package testify.bus;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.*;
+
+class SimpleBusImplTest {
+
+    @Test
+    void testMsgIsNotDone() throws ExecutionException, InterruptedException {
+        try (SimpleBusImpl simpleBus = new SimpleBusImpl()) {
+
+            // try an asynchronous get
+            final ExecutorService xs = Executors.newSingleThreadExecutor();
+            try {
+                final Future<String> msg = xs.submit(() -> simpleBus.get("msg"));
+                Assertions.assertFalse(msg.isDone());
+            } finally {
+                xs.shutdown();
+            }
+        }
+    }
+
+    @Test
+    void testMsgIsDone() throws ExecutionException, InterruptedException {
+        try (SimpleBusImpl simpleBus = new SimpleBusImpl()) {
+
+            // try an asynchronous get
+            final ExecutorService xs = Executors.newSingleThreadExecutor();
+            try {
+                final Future<String> msg = xs.submit(() -> simpleBus.get("msg"));
+                simpleBus.put("msg", "hello");
+                Assertions.assertTrue(msg.isDone());
+            } finally {
+                xs.shutdown();
+            }
+        }
+    }
+
+    @Test
+    void testMsgIsCorrect() throws ExecutionException, InterruptedException {
+        try (SimpleBusImpl simpleBus = new SimpleBusImpl()) {
+
+            // try an asynchronous get
+            final ExecutorService xs = Executors.newSingleThreadExecutor();
+            try {
+                final Future<String> msg = xs.submit(() -> simpleBus.get("msg"));
+                simpleBus.put("msg", "hello");
+                Assertions.assertEquals("hello", msg.get());
+                System.out.println("Correctly retrieved message: " + msg.get());
+            } finally {
+                xs.shutdown();
+            }
+        }
+    }
+
+    @Test
+    void testCheckCallbackHasNotBeenCalled() throws ExecutionException, InterruptedException {
+        try (SimpleBusImpl simpleBus = new SimpleBusImpl()) {
+
+            // try an asynchronous get
+            final ExecutorService xs = Executors.newSingleThreadExecutor();
+            try {
+                final Future<String> msg = xs.submit(() -> simpleBus.get("msg"));
+                simpleBus.put("msg", "hello");
+                System.out.println("Correctly retrieved message: " + msg.get());
+            } finally {
+                xs.shutdown();
+            }
+
+            {
+                // register a callback
+                CompletableFuture<String> msg = new CompletableFuture<>();
+                simpleBus.onMsg("msg", msg::complete);
+                // check the callback has not been called
+                Assertions.assertFalse(msg.isDone());
+            }
+        }
+    }
+
+    @Test
+    void testCheckCallbackHasBeenCalledCorrectly() throws ExecutionException, InterruptedException {
+        try (SimpleBusImpl simpleBus = new SimpleBusImpl()) {
+
+            // try an asynchronous get
+            final ExecutorService xs = Executors.newSingleThreadExecutor();
+            try {
+                final Future<String> msg = xs.submit(() -> simpleBus.get("msg"));
+                simpleBus.put("msg", "hello");
+                System.out.println("Correctly retrieved message: " + msg.get());
+            } finally {
+                xs.shutdown();
+            }
+
+            {
+                // register a callback
+                CompletableFuture<String> msg = new CompletableFuture<>();
+                simpleBus.onMsg("msg", msg::complete);
+                // put a new message
+                simpleBus.put("msg", "world");
+                // wait for the callback to be called
+                // check the callback has been called correctly
+                Assertions.assertEquals("world", msg.get());
+                System.out.println("Correctly retrieved message: " + msg.get());
+            }
+        }
+    }
+}
